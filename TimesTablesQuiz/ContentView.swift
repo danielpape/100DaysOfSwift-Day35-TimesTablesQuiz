3cab86b1761d22d2f2aebcadba1a60feefb60ba5
Daniel Pape <danielwpape@gmail.com>
2021-04-20 16:53:57 +0100

    Initial Commit

---
+++ TimesTablesQuiz/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TimesTablesQuiz
+//
+//  Created by Daniel Pape on 20/04/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TimesTablesQuiz/TimesTablesQuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimesTablesQuizApp.swift
+//  TimesTablesQuiz
+//
+//  Created by Daniel Pape on 20/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct TimesTablesQuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
