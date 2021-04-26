1bb7b642dcc11d632c03e4a5bcac4ad84e420691
Daniel Pape <danielwpape@gmail.com>
2021-04-26 21:01:48 +0100

    Quiz working MVP

--- TimesTablesQuiz/ContentView.swift
+++ TimesTablesQuiz/ContentView.swift
@@ -9,14 +9,57 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var selectedTimesTable = 2
-    @State private var questions = ["5","10","20","All"]
+    @State private var selectedTimesTable = 5
+    @State private var questions = ["5","10","20"]
     @State private var selectedQuestion = 1
     
     @State private var activeQuestion = 1
-    @State private var answer = "0"
+    @State private var answer = ""
+    @State private var correctAnswers = 0
 
+
+    @State private var multipliedBy = Int.random(in: 1...10)
     @State private var quizActive = false
+    
+    @State private var showingResults = false
+    @State private var endMessage = ""
+    
+    func reset(){
+        answer = ""
+        showingResults = false
+        quizActive = false
+        activeQuestion = 1
+        correctAnswers = 0
+    }
+    
+    func setEndText(){
+        if(correctAnswers == Int(questions[selectedQuestion])){
+            endMessage = "A perfect score!"
+            return
+        }
+        if(correctAnswers > Int(questions[selectedQuestion])!/2){
+            endMessage = "Great Score! Well done!"
+            return
+        }
+        if(correctAnswers >= 3){
+            endMessage = "Please keep practising!"
+            return
+        }
+    }
+    
+    func submitAnswer(){
+        if(Int(answer) == selectedTimesTable * multipliedBy ){
+            correctAnswers += 1
+        }
+        if (activeQuestion == Int(questions[selectedQuestion])){
+            setEndText()
+            showingResults.toggle()
+            quizActive = false
+        }
+        answer = ""
+        activeQuestion += 1
+        multipliedBy = Int.random(in: 1...10)
+    }
     
     var body: some View {
         Group{
@@ -25,13 +68,16 @@ struct ContentView: View {
                     if(quizActive){
                         Form{
                             Section{
-                        Text("What is 2x2?")
+                        Text("What is \(selectedTimesTable)x\(multipliedBy)?")
                             .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
                                 TextField("Enter your answer",text: $answer)
                                     .keyboardType(.numberPad)
                             .navigationTitle("Question \(activeQuestion)")
+                                    .navigationBarItems(trailing: Text("\(correctAnswers) / \(activeQuestion-1)"))
                             }
                         }
+                        
+                        
                     }else{
                     Form{
                         Section(header:Text("Which times table?")){
@@ -41,7 +87,7 @@ struct ContentView: View {
                         }
                         Section(header:Text("How many questions?")){
                             Picker("How many questions?", selection: $selectedQuestion){
-                                ForEach(0..<4){
+                                ForEach(0..<questions.count){
                                     Text("\(self.questions[$0])")
                                 }
                                 
@@ -56,6 +102,9 @@ struct ContentView: View {
                     
                     
                 }
+                .alert(isPresented: $showingResults, content: {
+                    Alert(title: Text("You got \(correctAnswers) out of \(questions[selectedQuestion])"), message: Text("\(endMessage)"), dismissButton: .default(Text("OK"),action: reset))
+                })
                 VStack{
                     Spacer()
                     if(!quizActive){
@@ -70,7 +119,7 @@ struct ContentView: View {
                 .clipShape(RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/))
                 } else {
                     Button("Enter Answer"){
-                        activeQuestion += 1
+                        submitAnswer()
                     }
                     .frame(maxWidth: 160)
                     .padding(.all)
@@ -81,8 +130,10 @@ struct ContentView: View {
                 }
                 }.padding()
             }
+            
         }
     }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
